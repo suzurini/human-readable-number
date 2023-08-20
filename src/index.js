b9622d4edc5f57868121bc981d906083968c3155
suzurini <valloforr@gmail.com>
2023-08-20 21:34:18 +0300

    fix: index of array to let

--- src/index.js
+++ src/index.js
@@ -2,29 +2,36 @@ module.exports = function toReadable (number) {
     let numberSmall = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let numberRound = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'thousand'];
     let humanReadable = 'zero';
+    let numberLength = toString(number).length;
+        console.log('numberLength = '+numberLength);
     if (number < 20) { 
         humanReadable = numberSmall[number-1]; 
     }
     else 
         if (toString(number).length == 2 ) {
-            if (+toString(number).slice(-1) == 0) {
-                humanReadable = numberRound[+toString(number).slice(0, 1) - 2];
+            let firstSymbol = +toString(number).slice(0, 1);
+            let lastSymbol = +toString(number).slice(-1);
+            if (lastSymbol == 0) {
+                humanReadable = numberRound[firstSymbol - 2];
             }
             else {
-                humanReadable = numberRound[+toString(number).slice(0, 1) - 1] + ' ' + numberSmall[+toString(number).slice(-1) - 1];
+                humanReadable = numberRound[firstSymbol - 1] + ' ' + numberSmall[lastSymbol - 1];
             }
         }
         else
             if (toString(number).length == 3) {
-                if (+toString(number).slice(-1) == 0) {
-                    humanReadable = numberSmall[+toString(number).slice(0, 1) - 1] + ' hundred ' + numberRound[+toString(number).slice(1, 2) - 1];
+                let firstSymbol = +toString(number).slice(0, 1);
+                let middleSymbol =  +toString(number).slice(1, 2);
+                let lastSymbol = +toString(number).slice(-1);
+                if (lastSymbol == 0) {
+                    humanReadable = numberSmall[firstSymbol - 1] + ' hundred ' + numberRound[middleSymbol - 1];
                 }
                 else 
-                    if (+toString(number).slice(1, 2) == 0) {
-                        humanReadable = numberSmall[+toString(number).slice(0, 1) - 1] + ' hundred ' + numberSmall[+toString(number).slice(-1) - 1];
+                    if (middleSymbol == 0) {
+                        humanReadable = numberSmall[firstSymbol - 1] + ' hundred ' + numberSmall[lastSymbol - 1];
                     }
                     else {
-                        humanReadable = numberSmall[+toString(number).slice(0, 1) - 1] + ' hundred ' + numberRound[+toString(number).slice(1, 2) - 1] + ' ' + numberSmall[+toString(number).slice(-1) - 1];
+                        humanReadable = numberSmall[firstSymbol - 1] + ' hundred ' + numberRound[middleSymbol - 1] + ' ' + numberSmall[lastSymbol - 1];
                     }
             }
     return humanReadable;
